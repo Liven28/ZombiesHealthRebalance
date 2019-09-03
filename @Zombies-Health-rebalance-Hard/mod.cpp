3cf8842c01a8cdcee4d50118cf0ededa8f84160b
liven <liven28@free.fr>
2019-09-03 15:17:57 +0200

    V 1.05.152376 XP

---
+++ @Zombies-Health-rebalance-Hard/mod.cpp
@@ -0,0 +1,10 @@
+name = "Zombies Health Rebalance"; 
+picture = 	"Zombies-Health-rebalance/Icons/IconZombies_Health_Rebalance.paa";		// picture in expanded description
+logoSmall = "Zombies-Health-rebalance/Icons/IconZombies_Health_Rebalance.paa";		// icon next to mod name when description is not expanded
+logo = 		"Zombies-Health-rebalance/Icons/IconZombies_Health_Rebalance.paa";		// logo below game menu
+logoOver = 	"Zombies-Health-rebalance/Icons/IconZombies_Health_Rebalance.paa";		// on mouse hover over logo
+tooltip = "Zombie categories have different health";					// tool tip on mouse hover
+overview = "Learn which zombies are the stronger and choose if you can fight them";
+action = "https://steamcommunity.com/sharedfiles/filedetails/?id=1633167429";	// link
+author = "Liven";
+version = "";	
